b5d9e41d4b5ac90b260f60816c97260385be6a42
sadegh ghasemnejad <ghasemnejad.sadegh@gmail.com>
2023-01-23 15:42:06 +0330

    Refactor:add comments to permissions components

--- src/pages/Dashboard/permissions/permissions.js
+++ src/pages/Dashboard/permissions/permissions.js
@@ -13,9 +13,7 @@ import {
   Button,
 } from "reactstrap";
 import { Colxx } from "components/common/CustomBootstrap";
-import Switch from "rc-switch";
 import "rc-switch/assets/index.css";
-import SurveyApplicationMenu from "containers/applications/SurveyApplicationMenu";
 import HeaderLayout from "containers/ui/headerLayout";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -24,32 +22,45 @@ import {
   searchPermissions,
   addPermissions,
 } from "redux-toolkit/permissionsSlice";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { toast } from "react-toastify";
 import checkPersian from "components/custom/validation/checkPersian";
 import checkCountCharacters from "components/custom/validation/checkCountCharacters";
 import checkUnique from "components/custom/validation/checkUnique";
 const Permissions = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
+  // گرفتن اطلاعات از ریداکس
   const { loading, allPermissions, pageSize } = useSelector(
     (store) => store.permissions
   );
+  // تمام اطلاعات را در استیت زیر ذخیره کردیم
   const [permissions, setPermissions] = useState([]);
+  // ref اینپوت سرچ
   const searchInputRef = useRef();
+  // آیا این جدول کالپس دارد یا نه؟
   const [collapse, setCollapse] = useState([]);
+  // آیدی دیتایی که میخواهیم ان را ویرایش کنیم
   const [id, setId] = useState();
+  // دیتای اولیه برای فرم ویرایش
   const [editData, setEditData] = useState({});
+  // دیتای نهایی جهت ویرایش ان دیتا
   const [editDataValue, setEditDataValue] = useState({});
+  // استیت مدال اضافه کردن دیتا
   const [isModal, setIsModal] = useState(false);
+  // استیت ندال ویرایش دیتا
   const [isModal2, setIsModal2] = useState(false);
+  // اطلاعات اولیه جهت اضافه کردن دیتا
   const [addData, setAddData] = useState({
     name: "",
     p_name: "",
     description: "",
   });
+  // دیتایی که موقع کالپس باید نمیاش دهد
   const [collapseData, setCollapseData] = useState([
     { type: "textarea", value: [] },
   ]);
+  // استیت های ولیدیشن فرم ها
   const [pNameValidation, setPNameValidation] = useState({
     status: true,
     message: "",
@@ -62,7 +73,7 @@ const Permissions = () => {
     status: true,
     message: "",
   });
-  const history = useHistory();
+  // دیتا های اصلی جدول
   const cols = useMemo(
     () => [
       {
@@ -148,12 +159,16 @@ const Permissions = () => {
     ],
     [collapse, id]
   );
+  // گرفتن اطلاعات اصلی
   useEffect(() => {
     fetchPermissions();
   }, [fetchPermissions]);
+  // ریختن دیتاهای اصلی داخل استیت
   useEffect(() => {
     setPermissions(allPermissions);
   }, [allPermissions]);
+
+  // گرفتن اطلاعات اولیه برای فرم ویرایش
   useEffect(() => {
     const data = allPermissions.find((p) => p.id == id);
     if (!data) return;
@@ -163,6 +178,7 @@ const Permissions = () => {
       description: data?.description === null ? "" : data.description,
     });
   }, [id]);
+  // گرفتن اطعات اولیه از دیتا بیس
   const fetchPermissions = async () => {
     try {
       await dispatch(getAllPermissions());
@@ -170,6 +186,7 @@ const Permissions = () => {
       throw err;
     }
   };
+  // تابع هندل کردن سرچ
   const searchHandler = async (e, searchId) => {
     e.preventDefault();
 
@@ -187,6 +204,7 @@ const Permissions = () => {
       throw err;
     }
   };
+  // تابع هندل کردن ذخیره ویرایش
   const saveChangeHandler = async () => {
     try {
       const res = await dispatch(
@@ -207,6 +225,7 @@ const Permissions = () => {
       throw err;
     }
   };
+  // تابع هندل کردن ذخیره دیتای جدید
   const addPermissionHandler = async () => {
     try {
       if (addData.p_name.length > 125) {
@@ -228,6 +247,7 @@ const Permissions = () => {
       throw err;
     }
   };
+  // آدر صحفه کنونی برای برد کرامب
   const match = [
     {
       path: "/",
@@ -244,6 +264,7 @@ const Permissions = () => {
       {!loading && (
         <div>
           <Colxx lg="12" xl="12">
+            {/* مدال ویرایش */}
             <Modal
               isOpen={isModal}
               size="lg"
@@ -388,6 +409,7 @@ const Permissions = () => {
                 </Button>{" "}
               </ModalFooter>
             </Modal>
+            {/* مدال اضافه کردن دیتای جدید */}
             <Modal
               isOpen={isModal2}
               size="lg"
@@ -546,6 +568,17 @@ const Permissions = () => {
               </ModalFooter>
             </Modal>
             <Card className="mb-4 p-5">
+              {/* برد کرامب و دکمه سرج و اضافه کردن در کامپوننت پایین قرار دارد 
+            ورودی ها : 
+            -title:عنوان صفحه
+            -match:برای برد کرامپ
+            -onSearch:تابع هندل کردن سرچ
+            -hasSearch:آیا این صفحه گزینه ای برای سرچ کردن دارد یا نه
+            -searchInputRef:ref برای اینپوت سرچ
+            -onAdd:تابع برای وقتی که کاربر روی دکمه اضافه کردن زد
+            -searchOption:آپشن های مختلف برای سرچ کردن که شامل ایدی و نام میباشد
+            -
+            */}
               <HeaderLayout
                 title="مدیریت دسترسی ها"
                 addName="افزودن دسترسی جدید"
@@ -567,6 +600,13 @@ const Permissions = () => {
                 }}
                 match={match}
               />
+              {/* برای نشان دادن جدول استفاده میشود 
+            ورودی ها:
+            -cols:دیتا های ستون ها 
+            -data:دیتا های اصلی
+            -isCollapse:آیا این جدول کالپس دارد؟
+            -collapseData:وقتی که کاربر کالپس را زد چه دیتاهایی در کالپس نشان دهد
+            */}
               <Table
                 cols={cols}
                 data={permissions}
