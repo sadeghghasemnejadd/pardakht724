50e91f2ffc9eb5e2297bc73b159160e32eae2c6a
sadegh <ghasemnejad.sadegh@gmail.com>
2023-01-26 00:59:09 +0330

    Refactor: add services-category/services-category.js comments

--- src/pages/Dashboard/service-categories/Service-categories.js
+++ src/pages/Dashboard/service-categories/Service-categories.js
@@ -28,34 +28,50 @@ import { toast } from "react-toastify";
 import HeaderLayout from "containers/ui/headerLayout";
 const ServiceCategories = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
+  // گرفتن اطلاعات از ریداکس
   const { loading, allServiceCategories } = useSelector(
     (store) => store.serviceCategories
   );
+  // استیت ذخیره اطلاعات در استیت
   const [serviceCategories, setServiceCategories] = useState([]);
+  // ref اینپوت سرچ
   const searchInputRef = useRef();
+  // آیا جدول کالپس داره؟
   const [collapse, setCollapse] = useState([]);
+  // آیدی دیتایی که میخواد ویرایش شود
   const [id, setId] = useState();
+  // دیتاهای اولیه ویرایش
   const [editData, setEditData] = useState({});
+  // دیتا های نهایی ویرایش
   const [editDataValue, setEditDataValue] = useState({});
+  // مدال اضافه کردن دیتا
   const [isModal, setIsModal] = useState(false);
+  // مدال ویرایش دیتا
   const [isModal2, setIsModal2] = useState(false);
+  // ref اینپوت آیکون
   const iconRef = useRef();
+  // ref اینپوت آیکون
   const editIconRef = useRef();
+  // استیت آیکون
   const [addIcon, setAddIcon] = useState();
+  // استیت ویرایش آیکون
   const [editIcon, setEditIcon] = useState();
+  // دیتا های اولیه برای ذخیره دیتا
   const [addData, setAddData] = useState({
     name: "",
     description: "",
     url: "",
     is_active: 1,
   });
+  // دیتا هایی که موقع کالپس جدول نمایش میدهد
   const [collapseData, setCollapseData] = useState([
     {
       type: "textarea",
       value: [],
     },
   ]);
-  const history = useHistory();
+  // برد کرامب صحفه
   const match = [
     {
       path: "/",
@@ -66,6 +82,7 @@ const ServiceCategories = () => {
       text: "دسته بندی سرویس ها",
     },
   ];
+  // محتوای اصلی جدول
   const cols = useMemo(
     () => [
       {
@@ -183,12 +200,15 @@ const ServiceCategories = () => {
     ],
     [collapse, id]
   );
+  // گرفتن دیتا های اصلی جدول
   useEffect(() => {
     fetchServiceCategories();
   }, [fetchServiceCategories]);
+  // ذخیره دیتا ها در استیت جداگونه
   useEffect(() => {
     setServiceCategories(allServiceCategories);
   }, [allServiceCategories]);
+  // دیتا های اولیه برای ویرایش
   useEffect(() => {
     const data = allServiceCategories.find((p) => p.id == id);
     if (!data) return;
@@ -199,6 +219,7 @@ const ServiceCategories = () => {
       is_active: data?.is_active === null ? false : data.is_active,
     });
   }, [id]);
+  // تابع گرفتن دیتا ها از دیتابیس
   const fetchServiceCategories = async () => {
     try {
       await dispatch(getAllServiceCategories());
@@ -206,7 +227,7 @@ const ServiceCategories = () => {
       throw err;
     }
   };
-
+  // تابع هندل کردن سرچ
   const searchHandler = async (e) => {
     e.preventDefault();
 
@@ -218,6 +239,7 @@ const ServiceCategories = () => {
       throw err;
     }
   };
+  // تابع هندل کردن اضافه کردن دیتا
   const addServiceCategoriesHandler = async () => {
     try {
       if (addData.name.length > 127) {
@@ -249,6 +271,7 @@ const ServiceCategories = () => {
       throw err;
     }
   };
+  // تابع هندل کردن ذخیره اطلاعات ویرایش شده
   const saveChangeHandler = async () => {
     try {
       const data = editIcon
@@ -278,12 +301,14 @@ const ServiceCategories = () => {
       throw err;
     }
   };
+  // تابع اضافه کردن آیکون
   const uploadIcon = (e) => {
     if (iconRef.current) {
       const file = e.target.files;
       setAddIcon(file[0]);
     }
   };
+  // تابع اضافه کردن آیکون ویرایش شده
   const uploadEditIcon = (e) => {
     if (iconRef.current) {
       const file = e.target.files;
@@ -296,6 +321,17 @@ const ServiceCategories = () => {
       {!loading && (
         <Colxx lg="12" xl="12">
           <Card className="mb-4 p-5">
+            {/* برد کرامب و دکمه سرج و اضافه کردن در کامپوننت پایین قرار دارد 
+            ورودی ها : 
+            -title:عنوان صفحه
+            -match:برای برد کرامپ
+            -onSearch:تابع هندل کردن سرچ
+            -hasSearch:آیا این صفحه گزینه ای برای سرچ کردن دارد یا نه
+            -searchInputRef:ref برای اینپوت سرچ
+            -onAdd:تابع برای وقتی که کاربر روی دکمه اضافه کردن زد
+            -searchOption:آپشن های مختلف برای سرچ کردن که شامل ایدی و نام میباشد
+            -
+            */}
             <HeaderLayout
               title="دسته بندی سرویس ها"
               addName="افزودن سرویس جدید"
@@ -313,12 +349,20 @@ const ServiceCategories = () => {
               ]}
               match={match}
             />
+            {/* برای نشان دادن جدول استفاده میشود 
+            ورودی ها:
+            -cols:دیتا های ستون ها 
+            -data:دیتا های اصلی
+            -isCollapse:آیا این جدول کالپس دارد؟
+            -collapseData:وقتی که کاربر کالپس را زد چه دیتاهایی در کالپس نشان دهد
+            */}
             <Table
               cols={cols}
               data={serviceCategories}
               isCollapse={collapse}
               collapseData={collapseData}
             />
+            {/* مدال اضافه کردن دیتا */}
             <Modal
               isOpen={isModal}
               size="lg"
@@ -419,6 +463,7 @@ const ServiceCategories = () => {
                 </Button>
               </ModalFooter>
             </Modal>
+            {/* مدال ویرایش دیتا */}
             <Modal
               isOpen={isModal2}
               size="lg"
